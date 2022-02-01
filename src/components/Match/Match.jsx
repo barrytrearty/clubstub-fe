6a9540ef6af67b2b3ed75850139cb4bbf8b42259
barrytrearty <85511553+barrytrearty@users.noreply.github.com>
2022-02-01 12:22:49 +0000

    Added extra column to match

--- src/components/Match/Match.jsx
+++ src/components/Match/Match.jsx
@@ -1,6 +1,6 @@
-import { withRouter, Redirect } from "react-router-dom";
+import { withRouter, Redirect, Link } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { Button, Container, Spinner, Modal } from "react-bootstrap";
+import { Button, Container, Spinner, Modal, Row, Col } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import axios from "axios";
 import StripeCheckout from "react-stripe-checkout";
@@ -121,50 +121,123 @@ const Match = ({ match }) => {
         </div>
       ) : (
         <div className=" pb-5">
+          <h2>Edit bit</h2>
           <img src={matchObj.image} alt="" className="match-image" />
-          <div id="match-details-holder">
-            <h2>
-              {matchObj.competition.description} {matchObj.description}
-            </h2>
+          <Container id="match-details-holder">
+            <Row>
+              {" "}
+              <Col xs={12} md={6}>
+                {" "}
+                <h2>Description: </h2>
+                <h3>
+                  {matchObj.competition.description} {matchObj.description}
+                </h3>
+                <div className="match-detail">
+                  <img
+                    src={matchObj.homeTeam.crest}
+                    alt=""
+                    className="match-team-image"
+                  />
+                  <span>
+                    {matchObj.homeTeam.name} vs {matchObj.awayTeam.name}
+                  </span>
 
-            <div className="match-detail">
-              <img
-                src={matchObj.homeTeam.crest}
-                alt=""
-                className="match-team-image"
-              />
-              <span>
-                {matchObj.homeTeam.name} vs {matchObj.awayTeam.name}
-              </span>
+                  <img
+                    src={matchObj.awayTeam.crest}
+                    alt=""
+                    className="match-team-image"
+                  />
+                </div>
+                <h2>Venue: </h2>
+                <div className="match-detail">{matchObj.venue}</div>
+                <h2>Date and Time: </h2>
+                <div className="match-detail">Time: {matchObj.time}</div>
+                <div className="match-detail">Date: {matchObj.displayDate}</div>
+                <h2>Entry Fee: </h2>
+                <div className="match-detail">
+                  Entry Fee: €{matchObj.entryFee}
+                </div>
+                <h2>Tickets remaining: </h2>
+                <div className="match-detail">{matchObj.capacity}</div>
+                <div className="ticket-wrapper my-4">
+                  <button
+                    className="ticket-button"
+                    onClick={handleShow}
+                    id="ticketButton"
+                  >
+                    GET TICKETS
+                  </button>
+                </div>
+              </Col>
+              <Col xs={12} md={6}>
+                <h2>Featured</h2>
+                <h3>Teams</h3>
+                <Row>
+                  <Col xs={6}>
+                    {" "}
+                    <div className="card-section-card">
+                      <Link to={`editClub/${matchObj.homeTeam._id}`}>
+                        <img
+                          src={matchObj.homeTeam.crest}
+                          alt=""
+                          className="card-img"
+                        />
+                      </Link>
+                      <div className="team-section-card-info">
+                        <Link to={`editClub/${matchObj.homeTeam._id}`}>
+                          <span className="team-section-card-title">
+                            {matchObj.homeTeam.name}
+                          </span>
+                        </Link>
+                      </div>
+                    </div>
+                  </Col>
+                  <Col xs={6}>
+                    {" "}
+                    <div className="card-section-card">
+                      <Link to={`editClub/${matchObj.homeTeam._id}`}>
+                        <img
+                          src={matchObj.awayTeam.crest}
+                          alt=""
+                          className="card-img"
+                        />
+                      </Link>
+                      <div className="team-section-card-info">
+                        <Link to={`editClub/${matchObj.homeTeam._id}`}>
+                          <span className="team-section-card-title">
+                            {matchObj.awayTeam.name}
+                          </span>
+                        </Link>
+                      </div>
+                    </div>
+                  </Col>
+                </Row>
 
-              <img
-                src={matchObj.awayTeam.crest}
-                alt=""
-                className="match-team-image"
-              />
-            </div>
-
-            <div className="match-detail">Venue: {matchObj.venue}</div>
-            <div className="match-detail">Throw in: {matchObj.time}</div>
-
-            <div className="match-detail">Date: {matchObj.displayDate}</div>
-
-            <div className="match-detail">Entry Fee: €{matchObj.entryFee}</div>
-
-            <div className="match-detail">
-              Tickets remaining: {matchObj.capacity}
-            </div>
-
-            <div className="ticket-wrapper my-4">
-              <button
-                className="ticket-button"
-                onClick={handleShow}
-                id="ticketButton"
-              >
-                GET TICKETS
-              </button>
-            </div>
-          </div>
+                <h3>Competition</h3>
+                <Row>
+                  <Col>
+                    <Link to={`/matches`}>
+                      <div className="card-section-card">
+                        <img
+                          src={matchObj.competition.image}
+                          alt=""
+                          className="card-img"
+                        />
+                        {/* </Link> */}
+                        {/* <Link to={`${matchOrEdit}/${match._id}`}> */}
+                        <div>
+                          <div className="comp-section-card-title">
+                            {matchObj.competition.description}
+                          </div>
+                          {/* <div>{comp.description}</div> */}
+                        </div>
+                      </div>
+                    </Link>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </Container>
           {/* <Button onClick={handleShow} id="ticketButton">
             Get Tickets
           </Button> */}
